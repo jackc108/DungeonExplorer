3fd3919a9f1761a593780629b6219c737422f055
jackc108 <29148578@students.lincoln.ac.uk>
2025-03-12 19:06:39 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,20 +8,31 @@ namespace DungeonExplorer
 {
     internal class Program
     {
-        public static Player currentPlayer = new Player();
+        //public static Player currentPlayer = new Player();
         static void Main(string[] args)
         {
-            Start();
+            // Create a new player with a name and health
+            Player player1 = new Player("Hero", 100);
+
+            // Create a new room with a description
+            Room room1 = new Room("A dark and damp room with a faint smell of mildew.");
+
+            // Display the players status
+            player1.DisplayPlayerStatus();
+
+            // Display the rooms description
+            room1.DisplayDescription();
+
+            Console.WriteLine("You find a sword and shield on the floor");
+
+            // Add an item to the players inventory
+            player1.AddToInventory("Sword");
+            player1.AddToInventory("Shield");
+
+            // Display te updated players inventory
+            player1.DisplayInventory();
+
         }
 
-        static void Start()
-        {
-            Console.WriteLine("Dungeon");
-            Console.WriteLine("Name: ");
-            currentPlayer.name = Console.ReadLine();
-            Console.WriteLine("You awake in a cold, stone, dark room. You seem to be having trouble remembering");
-            Console.WriteLine("You know your name is " + currentPlayer.name);
-            Console.ReadKey();
-        }
     }
 }
