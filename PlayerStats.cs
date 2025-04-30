fa629cedc6f1323e0e651a971301e869691d2403
jackc108 <29148578@students.lincoln.ac.uk>
2025-04-30 20:32:21 +0100

    Update and rename Player.cs to PlayerStats.cs

---
+++ PlayerStats.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    class PlayerStats
+    {
+        public int dmg = 10;
+        public int maxHp = 100;
+        public int hp = 100;
+        public int exp = 0;
+    }
+}
